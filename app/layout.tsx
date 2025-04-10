67a4ea2cfc13d10efcfdf55a5344547d00c0f14f
KyoTranKMA <quangdieuisme@gmail.com>
2025-04-10 12:01:57 +0700

    chore: update favicon UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
-import type { Metadata, Viewport } from 'next';
-import './globals.css';
+import type { Metadata, Viewport } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Trợ lý sen - Chăm sóc khách hàng',
-  description: 'Trợ lý sen - Chuyên viên chăm sóc khách hàng',
-  generator: 'v0.dev',
+  title: "Trợ lý sen - Chăm sóc khách hàng",
+  description: "Trợ lý sen - Chuyên viên chăm sóc khách hàng",
+  generator: "v0.dev",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export const viewport: Viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
@@ -20,7 +23,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en-US">
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body>{children}</body>
     </html>
   );
